c4e8cc7529e9e0f4f80af85d675e4940d05e7fef
Madeny Diawara <madeny@live.com>
2017-04-01 04:33:58 +0200

    Updates and bug

---
+++ resources/views/partial/head.blade.php
@@ -0,0 +1,152 @@
+<header>
+ 
+<div class="top-strip">
+<div class="container">
+ 
+<div class="language">
+<div class="dropdown">
+<a class="dropdown-toggle" id="dLabel1" role="button" data-toggle="dropdown" data-target="#" href="/page.html">
+Language
+<b class="caret"></b>
+</a>
+<ul class="dropdown-menu" role="menu" aria-labelledby="dLabel1">
+<li><a href="#">English</a></li>
+<li><a href="#">Spenish</a></li>
+<li><a href="#">Turkish</a></li>
+<li><a href="#">Arabic</a></li>
+</ul>
+</div>
+<div class="dropdown">
+<a class="dropdown-toggle" id="currency" role="button" data-toggle="dropdown" data-target="#" href="/page.html">
+Currency
+<b class="caret"></b>
+</a>
+<ul class="dropdown-menu" role="menu" aria-labelledby="currency">
+<li><a href="#">Dollar</a></li>
+<li><a href="#">Euro</a></li>
+<li><a href="#">Dirham</a></li>
+<li><a href="#">Rupees</a></li>
+<li><a href="#">Dinar</a></li>
+</ul>
+</div>
+</div>
+ 
+ 
+<div class="account">
+<ul>
+<li><a href="#">Subscribe</a></li>
+<li><a href="#">Sign Out</a></li>
+<li>
+<div class="dropdown">
+<a class="dropdown-toggle" id="account" role="button" data-toggle="dropdown" data-target="#" href="/page.html">
+My Account
+<b class="caret"></b>
+</a>
+<ul class="dropdown-menu" role="menu" aria-labelledby="account">
+<li><a href="#">Profile</a></li>
+<li><a href="#">Account Setting</a></li>
+<li><a href="#">Privacy Setting</a></li>
+</ul>
+</div>
+</li>
+</ul>
+</div>
+ 
+</div>
+</div>
+ 
+ 
+<div class="navigation-bar">
+<div class="container">
+<div class="logo">
+<a href="index.html"><img src="images/logo.png" alt=""></a>
+</div>
+<div class="cart">
+<ul>
+<li>
+<div class="search-bar"><i class="fa fa-search"></i></div>
+<div class="search-box">
+<input type="text" class="input-block-level">
+</div>
+</li>
+<li class="dropdown">
+<a class="dropdown-toggle" id="dLabel" role="button" data-toggle="dropdown" data-target="#" href="/page.html">
+<i class="fa fa-briefcase"></i><span>&euro;0</span>
+</a>
+<div class="dropdown-menu" role="menu" aria-labelledby="dLabel">
+<p>0 items in shopping bag</p>
+<p class="color">Unfortunately, your shopping bag is emtpy.</p>
+<a href="#" class="return">Return to the Shop</a>
+</div>
+</li>
+<li><i class="fa fa-star"></i><span>3</span></li>
+</ul>
+</div>
+<div class="navigation">
+<div class="navbar">
+<div class="navbar-inner">
+<div class="container">
+<button data-target=".nav-collapse" data-toggle="collapse" class="btn btn-navbar" type="button">
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+</button>
+<div class="nav-collapse collapse">
+<ul>
+<li><a href="javascript:void(0)">Home</a>
+<ul>
+<li><a href="index.html">Home Page Default</a></li>
+<li><a href="index-1.html">Home Page 1</a></li>
+<li><a href="index-2.html">Home Page 2</a></li>
+<li><a href="index-3.html">Home Page 3</a></li>
+<li><a href="index-4.html">Home Page 4</a></li>
+<li><a href="index-sidemenu.html">Home Page 5</a></li>
+</ul>
+</li>
+<li><a href="javascript:void(0)">Blog</a>
+<ul>
+<li><a href="blog.html">Blog</a></li>
+<li><a href="blog-detail.html">Blog Detail</a></li>
+</ul>
+</li>
+<li><a href="services.html">Services</a></li>
+<li><a href="javascript:void(0)">Events</a>
+<ul>
+<li><a href="events.html">Events</a></li>
+<li><a href="events-detail.html">Event Detail</a></li>
+</ul>
+</li>
+<li><a href="javascript:void(0)">Courses</a>
+<ul>
+<li><a href="courses.html">Courses</a></li>
+<li><a href="courses-detail.html">Course Detail</a></li>
+</ul>
+</li>
+<li><a href="javascript:void(0)">Pages</a>
+<ul>
+<li><a href="about-us.html">About us</a></li>
+<li><a href="404.html">Error 404</a></li>
+<li><a href="faq.html">FAQs</a></li>
+<li><a href="gallery.html">Gallery</a></li>
+<li><a href="news.html">News</a></li>
+<li><a href="our-lessons.html">Our Lessons</a></li>
+<li><a href="search-result.html">Search Result</a></li>
+<li><a href="signin.html">Sign In</a></li>
+<li><a href="student-login.html">Student Login</a></li>
+<li><a href="student-profile.html">Student Profile</a></li>
+<li><a href="teacher-profile.html">Teacher Profile</a></li>
+<li><a href="team.html">Our Team</a></li>
+<li><a href="edit-profile.html">Edit Your Profile</a></li>
+</ul>
+</li>
+<li><a href="contact-us.html">Contact Us</a></li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+ 
+</header>
