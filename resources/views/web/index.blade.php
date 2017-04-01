af391b018332af53e586e29f3b659ab7b8265a2e
Madeny Diawara <madeny@live.com>
2017-04-01 17:12:54 +0200

    Updates and bug

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -1,28 +1,4 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Runnel for Education</title>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
- 
-<link href="css/style.css" rel="stylesheet" media="screen">
-<link href="css/color.css" rel="stylesheet" media="screen">
-<link href="css/transitions.css" rel="stylesheet" media="screen">
- 
-<link href="css/bootstrap.css" rel="stylesheet" media="screen">
-<link href="css/bootstrap-responsive.css" rel="stylesheet" media="screen">
- 
-<link href="css/jquery.bxslider.css" rel="stylesheet" media="screen">
- 
-<link href="css/owl.carousel.css" rel="stylesheet">
- 
-<link href="css/font-awesome.min.css" rel="stylesheet" media="screen">
- 
-<link href="css/parallax.css" rel="stylesheet" type="text/css"/>
-</head>
-<body>
- 
-<div class="wrapper">
+
  <!-- Head start here -->
 @include('partial.head')
  <!--Head end here  -->
