e3b5311be612e9716c154bba71119eff0354205e
Madeny Diawara <madeny@live.com>
2017-04-01 17:42:58 +0200

    Updates and bug

---
+++ resources/views/partial/top.blade.php
@@ -0,0 +1,55 @@
+ 
+<div class="top-strip">
+<div class="container">
+ 
+<div class="language">
+<div class="dropdown">
+<a class="dropdown-toggle" id="dLabel1" role="button" data-toggle="dropdown" data-target="#" href="/page.html">
+Language
+<b class="caret"></b>
+</a>
+<ul class="dropdown-menu" role="menu" aria-labelledby="dLabel1">
+<li><a href="#">English</a></li>
+<li><a href="#">Spenish</a></li>
+<li><a href="#">Turkish</a></li>
+<li><a href="#">Arabic</a></li>
+</ul>
+</div>
+<div class="dropdown">
+<a class="dropdown-toggle" id="currency" role="button" data-toggle="dropdown" data-target="#" href="/page.html">
+Currency
+<b class="caret"></b>
+</a>
+<ul class="dropdown-menu" role="menu" aria-labelledby="currency">
+<li><a href="#">Dollar</a></li>
+<li><a href="#">Euro</a></li>
+<li><a href="#">Dirham</a></li>
+<li><a href="#">Rupees</a></li>
+<li><a href="#">Dinar</a></li>
+</ul>
+</div>
+</div>
+ 
+ 
+<div class="account">
+<ul>
+<li><a href="#">Subscribe</a></li>
+<li><a href="#">Sign Out</a></li>
+<li>
+<div class="dropdown">
+<a class="dropdown-toggle" id="account" role="button" data-toggle="dropdown" data-target="#" href="/page.html">
+My Account
+<b class="caret"></b>
+</a>
+<ul class="dropdown-menu" role="menu" aria-labelledby="account">
+<li><a href="#">Profile</a></li>
+<li><a href="#">Account Setting</a></li>
+<li><a href="#">Privacy Setting</a></li>
+</ul>
+</div>
+</li>
+</ul>
+</div>
+ 
+</div>
+</div>
